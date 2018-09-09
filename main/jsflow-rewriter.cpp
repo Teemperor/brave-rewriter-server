df290ba51145282f59a9eff3d64a8cf4f727f668
Raphael Isemann <teemperor@gmail.com>
2018-09-09 14:11:14 +0200

    Added example rewriter

---
+++ main/jsflow-rewriter.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "RewriteServer.h"
+
+class JSFlowRewriter : public RewriteServer {
+public:
+  JSFlowRewriter() : RewriteServer(getenv("JSFLOW_REWRITER")) {
+  }
+
+  std::string rewrite(const std::string& msg) override {
+    std::cout << msg << std::endl;
+    return "alert(\"Hello world!\");\n" + msg;
+  }
+};
+
+int main(int argc, char **argv) {
+  if (getenv("JSFLOW_REWRITER") == nullptr) {
+    std::cerr << "JSFLOW_REWRITER variable not set!\n";
+    std::cerr << "Run 'export JSFLOW_REWRITER=/an/absolute/path' in both this"
+                 " terminal and the one running brave (with the same path"
+                 " value for both)";
+    return 1;
+  }
+  JSFlowRewriter server;
+  for(;;) {
+    server.step();
+  }
+}
