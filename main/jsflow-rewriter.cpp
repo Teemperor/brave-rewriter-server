75ed24870091f015a749e431f96fcce90012cdf5
Raphael Isemann <teemperor@gmail.com>
2018-11-30 20:44:56 +0100

    Tried to avoid rewriting debug injected scripts

--- main/jsflow-rewriter.cpp
+++ main/jsflow-rewriter.cpp
@@ -14,7 +14,7 @@ class JSFlowRewriter : public RewriteServer {
   /// Source code we inject before we include the JSFlow source code.
   std::string JSFlowPrefix =
 R"js(
-jsflow = { console : {} };
+var jsflow = { console : {} };
 )js";
 
   std::string JSFlowSource;
@@ -34,12 +34,16 @@ jsflow.monitor.warn  = console.log;
   /// consider the corresponding V8 instance valid for our rewriting purposes.
   std::string ValidV8Needle = "!function(e){var t={};function r(n){if(t[n])"
                               "return t[n].exports;var o=t[n]={i:n,l:!1,export";
+  std::string DebuggerV8Needle = "\"use strict\";(function(InjectedScriptHost,inspectedGlobalObject,injectedScriptId)";
+  std::string DebuggerFuncNeedle = "(function anonymous";
 
   /// Represents a V8 instance in Chrome that is communicating with us.
   struct V8Instance {
     /// Whether the V8 instance is used for a website, and not just some
     /// internal V8 instance used for the dev tools, etc..
-    bool Valid;
+    bool ShouldRewrite = false;
+    bool IsDebuggerInstance = false;
+    bool InitializedJSFlow = false;
   };
 
   /// Maps UIDs to known V8 instances.
@@ -67,6 +71,10 @@ public:
     return JavaScriptEscaper::escape(Inpupt);
   }
 
+  bool hasNeedle(const std::string &Msg) {
+    return (Msg.rfind(ValidV8Needle, 0) == 0);
+  }
+
   // This is the callback we get from the RewriteServer where can rewrite the
   // message we received.
   std::string rewrite(const std::string& OriginalMsg) override {
@@ -92,28 +100,64 @@ public:
 
     // Check if the V8 instance we got is actually used for a website and not
     // for some internal Brave website.
-    bool IsNewV8Instance = (V8InstancesByUID.count(uid) == 0);
-    if (IsNewV8Instance) {
-      V8InstancesByUID[uid] = {(Msg.rfind(ValidV8Needle, 0) == 0)};
+    if (V8InstancesByUID.count(uid) == 0) {
+      std::cerr << "Found new v8 instance\n";
+      V8Instance NewInstance;
+      NewInstance.ShouldRewrite = hasNeedle(Msg);
+      NewInstance.IsDebuggerInstance = Msg.rfind(DebuggerV8Needle, 0) == 0;
+      V8InstancesByUID[uid] = NewInstance;
     }
 
-    // If the V8 instance isn't valid, then we don't need to rewrite.
-    if (!V8InstancesByUID[uid].Valid)
-      return Msg;
+    V8Instance &CurrentInstance = V8InstancesByUID[uid];
+
 
     // Print the first 200 characters to stdout. This is just for debugging
     // purposes.
     std::string Copy = Msg;
-    if (Copy.length() > 200)
-      Copy = Copy.substr(0, 200);
-    std::cout << "=========" << uid << "\n" << Copy << std::endl;
+    if (Copy.length() > 100) {
+      Copy = Copy.substr(0, 100);
+      Copy.append("...");
+    }
+    std::cerr << "\n<========" << uid << "\n" << Copy <<
+    "\n========>\n";
 
+    if (Msg.rfind(DebuggerV8Needle, 0) == 0) {
+      std::cerr << "Skipping debugger needle\n";
+      return Msg;
+    }
+
+    if (Msg.size() <= 25) {
+      std::cerr << "Skipping too short code\n";
+      return Msg;
+    }
+
+    if (CurrentInstance.IsDebuggerInstance && hasNeedle(Msg)) {
+      std::cerr << "Activaging rewriting in debuggin instance\n";
+      CurrentInstance.ShouldRewrite = true;
+    }
+
+    bool ShouldRewrite = CurrentInstance.ShouldRewrite;
+
+    if (Msg.rfind(DebuggerFuncNeedle, 0) == 0) {
+      std::cerr << "Not rewriting debugger function\n";
+      ShouldRewrite = false;
+    }
+
+    // If the V8 instance isn't valid, then we don't need to rewrite.
+    if (!ShouldRewrite) {
+      std::cerr << "Skipping because marked invalid\n";
+      return Msg;
+    }
+
+    std::cerr << "Rewriting\n";
 
     std::string Result;
 
     // If this is a V8 instance we haven't encountered before, we have to inject
     // the JSFlow source code and initializers.
-    if (IsNewV8Instance) {
+    if (!CurrentInstance.InitializedJSFlow) {
+      std::cerr << "Injecting JSFlow\n";
+      CurrentInstance.InitializedJSFlow = true;
       Result.append(JSFlowPrefix);
       Result.append(JSFlowSource);
       Result.append(JSFlowInitializers);
@@ -121,10 +165,23 @@ public:
 
     // Now we escape the original source code and let our JSFlow instance
     // execute it.
+    static unsigned ID = 0;
+    std::cerr << "Assigned ID " << (++ID) << std::endl;
+    Result.append("if (typeof jsflow === 'undefined') {\n"
+                  "  jsflow" + std::to_string(ID) + ".monitor.execute('');\n"
+                  "}\n");
     Result.append("\njsflow.monitor.execute(\"");
+    //Result.append("\njsflow" + std::to_string(ID) + ".monitor.execute(\"");
     std::string escaped = escape(Msg);
     Result.append(escaped);
-    Result.append("\");");
+    Result.append("\").value.value;");
+
+    Copy = Result;
+    if (Copy.length() > 150) {
+      Copy = Copy.substr(0, 100) + "[...]" + Copy.substr(Copy.size() - 50, 50);
+    }
+    std::cerr << "\n<========Result " << uid << "\n" << Copy <<
+              "\n========>\n";
 
     return Result;
   }
