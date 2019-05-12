1077ec386d70ddff81f0fcb479bc3e713c1b394f
Raphael Isemann <teemperor@gmail.com>
2019-05-12 19:06:02 +0200

    Fixed that we don't escape carriage return

--- src/JavaScriptEscaper.cpp
+++ src/JavaScriptEscaper.cpp
@@ -13,6 +13,10 @@ std::string JavaScriptEscaper::escape(const std::string &Input) {
       // Newline is translated into a backslash and a 'n' character (\n).
       Result.push_back('\\');
       Result.push_back('n');
+    } else if (c == '\r') {
+      // Carriage return is translated into a backslash and a 'r' character (\r).
+      Result.push_back('\\');
+      Result.push_back('r');
     } else {
       // All other characters stay untouched.
       Result.push_back(c);
